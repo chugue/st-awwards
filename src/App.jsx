a9d9c80b2fabaeb7417388ffe8ef1626a2234aa9
chugue <chugue85@gmail.com>
2025-07-22 20:03:24 +0900

    feat: hero section done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Hero from "./components/Hero.jsx";
+
+const App = () => (
+  <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <Hero />
+    <section className="z-0 min-h-screen bg-blue-500"></section>
+  </main>
+);
+
+export default App;
